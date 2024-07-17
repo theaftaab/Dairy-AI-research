b245e90bf7b33473224a817e5498cc1f19887236
theaftaab <aftaabhussaint@gmail.com>
2024-07-17 19:20:28 +0530

    Frame separator

---
+++ frame extraction.py
@@ -0,0 +1,47 @@
+import cv2
+import os
+
+
+def extract_frames(vid_path, frame_dir) -> None:
+    # Read the video from specified path
+    cam = cv2.VideoCapture(vid_path)
+
+    try:
+
+        # creating a folder named data
+        if not os.path.exists(frame_dir):
+            os.makedirs(frame_dir)
+
+        # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
+
+    # frame
+    currentframe = 0
+
+    while (True):
+
+        # reading from frame
+        ret, frame = cam.read()
+
+        if ret:
+            # if video is still left continue creating images
+            name = './data/frame' + str(currentframe) + '.jpg'
+            print('Creating...' + name)
+
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    extract_frames('Cow Rumination 2.mp4', './data/extracted_frames')

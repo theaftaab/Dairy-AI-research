9d6c63d19715b227660a9d362e0b07f4638835a2
AnshDevelops <singhal.ansh0503@gmail.com>
2024-07-19 13:27:15 +0530

    refactored code

--- frame extraction.py
+++ frame extraction.py
@@ -2,43 +2,28 @@ import cv2
 import os
 
 
-def extract_frames(vid_path, frame_dir) -> None:
-    # Read the video from specified path
+def extract_frames(vid_path: str, frame_dir: str) -> None:
     cam = cv2.VideoCapture(vid_path)
 
     try:
-
-        # creating a folder named data
         if not os.path.exists(frame_dir):
             os.makedirs(frame_dir)
 
-        # if not created then raise error
     except OSError:
         print('Error: Creating directory of data')
 
-    # frame
-    currentframe = 0
+    currentframe = 1
 
-    while (True):
-
-        # reading from frame
+    while True:
         ret, frame = cam.read()
-
         if ret:
-            # if video is still left continue creating images
             name = './data/frame' + str(currentframe) + '.jpg'
             print('Creating...' + name)
-
-            # writing the extracted images
             cv2.imwrite(name, frame)
-
-            # increasing counter so that it will
-            # show how many frames are created
             currentframe += 1
         else:
             break
 
-    # Release all space and windows once done
     cam.release()
     cv2.destroyAllWindows()
 

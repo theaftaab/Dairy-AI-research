a16d09490a826874583eb0c81d1bd1929ddb3b24
theaftaab <aftaabhussaint@gmail.com>
2024-07-20 12:12:19 +0530

    Adding processed rumination frames

--- frame extraction.py
+++ frame extraction.py
@@ -1,5 +1,44 @@
-import cv2
+import json
 import os
+import cv2
+
+
+def create_count_file(path):
+    """Creates a JSON file at the specified path with a 'count' key initialized to 0.
+
+    Args:
+      path: The path to the JSON file.
+    """
+    if not os.path.exists(path):
+        data = {'count': 0}
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=2)
+
+
+def read_count(path):
+    """Reads the 'count' value from the JSON file at the specified path.
+
+    Args:
+      path: The path to the JSON file.
+
+    Returns:
+      The 'count' value from the JSON file.
+    """
+    with open(path, 'r') as f:
+        data = json.load(f)
+        return data['count']
+
+
+def update_count(path, new_count):
+    """Updates the 'count' value in the JSON file at the specified path.
+
+    Args:
+      path: The path to the JSON file.
+      new_count: The new value for the 'count' key.
+    """
+    data = {'count': new_count}
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=2)
 
 
 def extract_frames(vid_path: str, frame_dir: str) -> None:
@@ -20,8 +59,14 @@ def extract_frames(vid_path: str, frame_dir: str) -> None:
 
     except OSError:
         print('Error: Creating directory of data')
+    path = 'count.json'
 
-    currentframe = 1
+    # Create the file if it doesn't exist
+    create_count_file(path)
+    # Read the current count
+    count = read_count(path)
+
+    currentframe = count
 
     while True:
         ret, frame = cam.read()
@@ -33,10 +78,13 @@ def extract_frames(vid_path: str, frame_dir: str) -> None:
             currentframe += 1
         else:
             break
+    update_count(path, currentframe)
 
     cam.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    extract_frames('samples/rumination2.mp4', 'data/extracted_frames')
+    for file in os.listdir('samples'):
+        extract_frames(f'samples/{file}', 'data/extracted_frames')
+    # extract_frames('samples/Rumination 3.mp4', 'data/extracted_frames')
